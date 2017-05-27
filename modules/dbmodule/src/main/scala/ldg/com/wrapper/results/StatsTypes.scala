42ca582afc6976ad0157b8d1264235888dd57453
Luis Dipotet <ldipotet.job@gmail.com>
2017-05-27 08:23:17 +0100

    Update StatsTypes.scala

--- modules/dbmodule/src/main/scala/ldg/com/wrapper/results/StatsTypes.scala
+++ modules/dbmodule/src/main/scala/ldg/com/wrapper/results/StatsTypes.scala
@@ -1,11 +1,10 @@
 package ldg.com.wrapper.results
 
 /**
-  * Created by ldipotet on 6/11/16.
+  * Created by ldipotet on 6/05/17.
   */
 object StatsTypes {
 
-  //type htmlTag = String
   type urlLink = String
   type teamName = String
   type playerName = String
