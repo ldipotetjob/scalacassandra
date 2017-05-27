224a0c262ed29e1f8eb893d0f618003b68b518e5
Luis Dipotet <ldipotet.job@gmail.com>
2017-05-27 07:58:39 +0100

    Update data_definition_languaje.sh

--- data_definition_languaje.sh
+++ data_definition_languaje.sh
@@ -1,3 +1,8 @@
  #!/bin/bash
  # This script is for build definition on a local cassandra cluster
- 
+ # schemas and tables in case that it doesn`t exist
+ # we must include in it or in others scripts in this paths
+ # we must include in other scripts in this path:
+ #    - code for add/update row for test
+ #    - code for add json to table for test
+ #    - code for stabilize the database for testing
